eb81cf0b2d7f41b873675550dc5822eb34700ad6
PC Filho <pconstanciofilho@gmail.com>
2020-03-13 16:01:05 -0300

    codigo Paulo

---
+++ TEAM 1/pandasPaulo/pandasPaulo.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+data_hyperbulk = pd.read_excel("F:\Paulo\Documents\GitHub\Trainee-Python-Course-2020\TEAM 1\pandasPaulo\Hyperbulk_input_v3.xlsx")
+data_sku_dic = pd.read_excel("F:\Paulo\Documents\GitHub\Trainee-Python-Course-2020\TEAM 1\pandasPaulo\SKU_dictionary.xlsx")
